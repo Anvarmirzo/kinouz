44490c498be9cfb57c62b5d336d9ee813bd390c5
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-08-21 04:57:15 +0500

    feat: completed movie page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type {AppProps} from 'next/app'
 import '../styles/sass/main.sass'
 
 function MyApp({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+    return <div className="app"><Component {...pageProps} /></div>
 }
 
 export default MyApp
